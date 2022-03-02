531d718bf32935c4eecda161715759acb1278ddd
RubanZ <ruban_1998@hotmail.com>
2022-03-02 17:25:04 +0300

    fix: мелкие правки feat: add yandex metric

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -45,6 +45,23 @@ function Footer() {
         `,
           }}>
       </Script>
+      {/* eslint-disable-next-line @next/next/inline-script-id */}
+      <Script strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+              ym(87687598, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+            });
+        `,}}>
+      </Script>
+      <noscript><div><img src="https://mc.yandex.ru/watch/87687598" alt="" /></div></noscript>
     </div>
   );
 }
