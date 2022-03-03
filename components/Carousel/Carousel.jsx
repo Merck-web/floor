cfd38b7bc1e3b8cab063561c3d0c4f7e7a9990b7
Your Name <you@example.com>
2022-03-03 22:59:48 +0500

    (

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -16,57 +16,59 @@ export default function Carousel() {
     autoplaySpeed: 5000,
   };
   return (
-    <div className={styles.carousel}>
-      <Slider {...settings}>
-        <div className={styles.item}>
-          <img className={styles.img} src={images[0]} />
-          <div className={styles.content}>
-            <p className={styles.title}>Мы</p>
-            <div className={styles.description}>
-              Авторская флористическая мастерская RECENS, оказываем полный
-              комплекс услуг по обслуживанию компаний в Москве.
-            </div>
-            <Bid>Оставить заявку</Bid>
-            <div className={styles.capture}>
-              * Оставьте заявку сейчас и получите подарок !
+    <div className="mainslider">
+      <div className={styles.carousel}>
+        <Slider {...settings}>
+          <div className={styles.item}>
+            <img className={styles.img} src={images[0]} />
+            <div className={styles.content}>
+              <p className={styles.title}>Мы</p>
+              <div className={styles.description}>
+                Авторская флористическая мастерская RECENS, оказываем полный
+                комплекс услуг по обслуживанию компаний в Москве.
+              </div>
+              <Bid>Оставить заявку</Bid>
+              <div className={styles.capture}>
+                * Оставьте заявку сейчас и получите подарок !
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.item}>
-          <img className={styles.img} src={images[1]} />
-          <div className={styles.content}>
-            <div className={styles.title2}>Мы организуем</div>
-            <div className={styles.description2}>
-              Авторская флористическая мастерская, оказывает полный комплекс
-              услуг по обслуживанию компаний в Москве.
+          <div className={styles.item}>
+            <img className={styles.img} src={images[1]} />
+            <div className={styles.content}>
+              <div className={styles.title2}>Мы организуем</div>
+              <div className={styles.description2}>
+                Авторская флористическая мастерская, оказывает полный комплекс
+                услуг по обслуживанию компаний в Москве.
+              </div>
+              <Bid>Получить презентацию</Bid>
             </div>
-            <Bid>Получить презентацию</Bid>
           </div>
-        </div>
-        <div className={styles.item}>
-          <img className={styles.img} src={images[2]} />
-          <div className={styles.content}>
-            <div className={styles.title2}>Мы разработаем</div>
-            <div className={styles.description2}>
-              индивидуальный стиль оформления для гостиниц, ресторанов,
-              магазинов, входных зон, а также продумаем и реализуем декор
-              кабинетов руководителей, персонала и переговорных комнат
+          <div className={styles.item}>
+            <img className={styles.img} src={images[2]} />
+            <div className={styles.content}>
+              <div className={styles.title2}>Мы разработаем</div>
+              <div className={styles.description2}>
+                индивидуальный стиль оформления для гостиниц, ресторанов,
+                магазинов, входных зон, а также продумаем и реализуем декор
+                кабинетов руководителей, персонала и переговорных комнат
+              </div>
+              <Bid>Получить презентацию</Bid>
             </div>
-            <Bid>Получить презентацию</Bid>
           </div>
-        </div>
-        <div className={styles.item}>
-          <img className={styles.img} src={images[3]} />
-          <div className={styles.content}>
-            <div className={styles.title2}>Мы поможем и сделаем</div>
-            <div className={styles.description2}>
-              декор для Ваших корпоративных активностей: презентации, выставки,
-              мероприятия, дни рождения и другие различные праздники
+          <div className={styles.item}>
+            <img className={styles.img} src={images[3]} />
+            <div className={styles.content}>
+              <div className={styles.title2}>Мы поможем и сделаем</div>
+              <div className={styles.description2}>
+                декор для Ваших корпоративных активностей: презентации,
+                выставки, мероприятия, дни рождения и другие различные праздники
+              </div>
+              <Bid>Получить презентацию</Bid>
             </div>
-            <Bid>Получить презентацию</Bid>
           </div>
-        </div>
-      </Slider>
+        </Slider>
+      </div>
     </div>
   );
 }
